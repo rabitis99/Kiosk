1a9721c131fd36da680ebb77182b3069038572d9
rabitis99 <kyungsu9914@gmail.com>
2025-03-11 23:18:22 +0900

    lv3

---
+++ src/Lv3/MenuItem.java
@@ -0,0 +1,15 @@
+package Lv3;
+
+public class MenuItem {
+    String name;
+    double price;
+    String commet;
+    public MenuItem(String name,double price,String commet){
+        this.name=name;
+        this.price=price;
+        this.commet=commet;
+    }
+    public void printMenuItem(){
+        System.out.println(name+"| W"+price+" |"+commet);
+    }
+}
